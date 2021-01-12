1006b1c3f2ce65295c4d2c0d08f74ee63e7a686d
kushtej <41020911+kushtej@users.noreply.github.com>
2021-01-12 19:04:17 +0530

    uncommented username is too short

--- accounts/static/accounts/js/main.js
+++ accounts/static/accounts/js/main.js
@@ -3,11 +3,11 @@ console.log('hello world!')
 function CheckUsername() {
     $.get('/signup/', { username: $('#username').val() },
         function (data) {
-            // var username = $.trim($('#username').val());
-            // if (username.length < 5) {
-            //     $('#availabality').css({ "color": "red" })
-            //     $('#availabality').html("Username too short!");
-            // } else {
+            var username = $.trim($('#username').val());
+            if (username.length < 5) {
+                $('#availabality').css({ "color": "red" })
+                $('#availabality').html("Username too short!");
+            } else {
                 if (data == "True") {
                     $('#availabality').css({ "color": "green" })
                     $('#availabality').html("Username available");
@@ -15,7 +15,7 @@ function CheckUsername() {
                     $('#availabality').css({ "color": "red" })
                     $('#availabality').html("Username not available");
                 }
-            //}
+            }
         });
 }
 function onChange() {
